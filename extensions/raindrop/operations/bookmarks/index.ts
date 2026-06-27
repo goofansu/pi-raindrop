060ff61eab2de4ed5742bfd68dcee4ac69c1a303
Yejun Su <goofan.su@gmail.com>
2026-06-27 18:55:49 +0800

    chore: remove unused package exports

--- extensions/raindrop/operations/bookmarks/index.ts
+++ extensions/raindrop/operations/bookmarks/index.ts
@@ -6,13 +6,6 @@ import { getOne } from "./get-one.ts";
 import { updateMany } from "./update-many.ts";
 import { updateOne } from "./update-one.ts";
 
-export { createMany } from "./create-many.ts";
-export { createOne } from "./create-one.ts";
-export { getMany } from "./get-many.ts";
-export { getOne } from "./get-one.ts";
-export { updateMany } from "./update-many.ts";
-export { updateOne } from "./update-one.ts";
-
 export const bookmarkOperations: RaindropOperation[] = [
   getOne,
   getMany,
